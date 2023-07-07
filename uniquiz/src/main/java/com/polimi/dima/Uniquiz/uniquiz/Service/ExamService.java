2a2a36f5f29363f47035271a5c7ae77cbb9ab519
giuliadauria <giulia.dauria@mail.polimi.it>
2023-07-07 18:23:58 +0200

    Fix addExam

--- uniquiz/src/main/java/com/polimi/dima/Uniquiz/uniquiz/Service/ExamService.java
+++ uniquiz/src/main/java/com/polimi/dima/Uniquiz/uniquiz/Service/ExamService.java
@@ -1,7 +1,9 @@
 package com.polimi.dima.Uniquiz.uniquiz.Service;
 
 import com.polimi.dima.Uniquiz.uniquiz.Domain.ExamEntity;
+import com.polimi.dima.Uniquiz.uniquiz.Domain.UserEntity;
 import com.polimi.dima.Uniquiz.uniquiz.Mappers.ExamMapper;
+import com.polimi.dima.Uniquiz.uniquiz.Mappers.UserMapper;
 import com.polimi.dima.Uniquiz.uniquiz.Model.Exam;
 import com.polimi.dima.Uniquiz.uniquiz.Model.ExamRequest;
 import com.polimi.dima.Uniquiz.uniquiz.Model.User;
@@ -13,6 +15,8 @@ import org.springframework.stereotype.Service;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -20,6 +24,11 @@ public class ExamService {
 
     private ExamRepository repository;
     private UserService userService;
+
+    public List<Exam> getExams(){
+        List<ExamEntity> exams = repository.findAll();
+        return exams.stream().map(ExamMapper.INSTANCE::fromEntity).collect(Collectors.toList());
+    }
 
 
     public User addExam(String userId, ExamRequest examRequest) {
@@ -34,9 +43,22 @@ public class ExamService {
             e.printStackTrace();
         }
         exam.setDate(date);
-        ExamEntity entity = ExamMapper.INSTANCE.toEntity(exam);
-        ExamEntity savedEntity = repository.save(entity);
-        Exam savedExam = ExamMapper.INSTANCE.fromEntity(savedEntity);
+        System.out.println(exam.getDate().toString());
+        Boolean toSave = true;
+        Exam savedExam = null;
+        //check if the exam already exists
+        for(Exam e : getExams()){
+            if(e.getSubjectId().compareTo(exam.getSubjectId()) == 0 && e.getDate().compareTo(exam.getDate()) == 0){
+                toSave = false;
+                savedExam = e;
+            }
+        }
+        //if it does not exist, save it and then add it to the user
+        if(toSave){
+            ExamEntity entity = ExamMapper.INSTANCE.toEntity(exam);
+            ExamEntity savedEntity = repository.save(entity);
+            savedExam = ExamMapper.INSTANCE.fromEntity(savedEntity); 
+        }        
         return userService.addExam(savedExam, userId);
     }
 }
